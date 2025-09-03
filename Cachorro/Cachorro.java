fc43bc92ffcc01dcab5e0fd92f58bdbeeb1a0c99
Matheus <113068587+mathewsph2@users.noreply.github.com>
2025-09-03 13:06:17 -0300

    Add files via upload

---
+++ Cachorro/Cachorro.java
@@ -0,0 +1,12 @@
+public class Cachorro
+{
+  String nome; 
+  int idade;
+  String raca;
+    public Cachorro(String nome, int idade, String raca) {
+    this.nome = nome; 
+    this.idade = idade;
+    this.raca = raca; 
+    
+  }
+}
---
+++ Cachorro/Principal.java
@@ -0,0 +1,12 @@
+public class Principal
+{
+  public static void main(String[] args) {
+    // Passando os valores diretamente no construtor
+    Cachorro cao = new Cachorro("Toto", 5, "Vira-lata"); 
+  
+     System.out.println(cao.nome);
+     System.out.println(cao.idade);
+     System.out.println(cao.raca);
+
+    }
+  }
